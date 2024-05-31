25e8b03a8e79cdf037428bd78f7505fb66bbe44c
FelanaRasata <77356909+FelanaRasata@users.noreply.github.com>
2024-05-31 22:07:58 +0300

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8889/api/',
+  // apiUrl: 'http://localhost:8889/api/',
   // apiUrl: 'http://localhost:3080/api/',
-  // apiUrl: "https://assignments-mbds.onrender.com/api/"
+  apiUrl: "https://assignments-mbds.onrender.com/api/"
   // other variables
 }
